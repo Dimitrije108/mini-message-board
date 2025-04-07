bfe3e1469d38bbe327d87d89fdc78c1a5374c996
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-04-07 13:02:41 +0200

    feat: implement /new form page for adding messages and / page for displaying all messages on a mini message board

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const path = require('node:path');
 const app = express();
 
 const indexRouter = require('./routes/indexRouter');
-
+// Setup html language and templates
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+// Setup assets (css, images, etc)
 const assetsPath = path.join(__dirname, 'public');
 app.use(express.static(assetsPath));
+// Parse form data into req.body
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
-
+// Setup routes
 app.use('/', indexRouter);
 
 app.listen(PORT, () => {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,9 +2,43 @@ const { Router } = require('express');
 
 const indexRouter = Router();
 
+let lastId = 2;
+
+const messages = [
+	{
+		id: 1,
+		text: 'Hello There!',
+		user: 'General Kenobi',
+		added: new Date(),
+	},
+	{
+		id: 2,
+		text: 'General Kenobi . . .',
+		user: 'General Grievous',
+		added: new Date(),
+	},
+];
+
+indexRouter.get('/new', (req, res) => {
+	res.render('form');
+});
+
+indexRouter.post('/new', (req, res) => {
+	messages.push({ 
+		id: ++lastId,
+		text: req.body.msg, 
+		user: req.body.name, 
+		added: new Date(),
+	});
+	res.redirect("/");
+});
+
+// indexRouter.get('/:msgId', (req, res) => {
+// 	res.render('message', { messages: messages });
+// });
+
 indexRouter.get('/', (req, res) => {
-	// insert controller
-	res.render('index');
+	res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = indexRouter;
